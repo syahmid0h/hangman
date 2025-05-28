1ddfc64b56cd24424f6abdce1274f579602a83c9
syahmid0h <syahmisaif@gmail.com>
2025-05-28 20:00:18 +0800

    ascii art

--- hangman.py
+++ hangman.py
@@ -2,4 +2,26 @@ import random
 
 words = ("red", 'yellow', 'green', 'blue', 'brown', 'pink', 'purple', 'black')
 
-123
+
+#ASCII ART
+hangman_art = {0:('   ',
+                  '   ',
+                  '   '), 
+               1:(' O ',
+                  '   ',
+                  '   '), 
+               2:(' O ',
+                  ' | ',
+                  '   '), 
+               3:(' O ',
+                  '/| ',
+                  '   '), 
+               4:(' O ',
+                  '/|\\',
+                  '   '), 
+               5:(' O ',
+                  '/|\\',
+                  '/  '), 
+               6:(' O ',
+                  '/|\\',
+                  '/ \\ ')}
